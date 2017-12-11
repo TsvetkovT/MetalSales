8b1f59283ce018e9e9b738f1c8d87efdede67e57
TsvetkovT <tsvetelin.p.tsvetkov@gmail.com>
2017-12-12 00:52:21 +0200

    adding flask-admin and configuring wp_users

--- metalsales.py
+++ metalsales.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging
 from data import Categories
 from flask_mysqldb import MySQL
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+import os
+
 
 #define flask app
 app = Flask(__name__)
+#
+app.secret_key = os.urandom(24)
+
 
 #Config MySQL
 
@@ -15,6 +22,31 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Bene1979'
 app.config['MYSQL_DB'] = 'trade'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#Config of flask-admin
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Bene1979@127.0.0.1/trade'
+app.config['secret key'] = '1q2w3e4r'
+db = SQLAlchemy(app)
+
+admin = Admin(app)
+
+class wp_users(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    user_login = db.Column(db.String(60))
+    user_pass = db.Column(db.String(64))
+    user_nicename = db.Column(db.String(50))
+    user_email = db.Column(db.String(100))
+    user_url = db.Column(db.String(100))
+    user_registered = db.Column(db.DATETIME(timezone=False))
+    user_activation_key = db.Column(db.String(60))
+    user_status = db.Column(db.Integer)
+    display_name = db.Column(db.String(250))
+
+
+
+
+admin.add_view(ModelView(wp_users, db.session))
 
 
 
@@ -55,4 +87,5 @@ def products():
 
 #running app parameters
 if __name__ == '__main__':
+
     app.run(debug=True, host='0.0.0.0')
