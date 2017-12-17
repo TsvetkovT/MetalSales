60793db599191b8e3107413392e2a7d19e63274f
TsvetkovT <tsvetelin.p.tsvetkov@gmail.com>
2017-12-17 12:33:45 +0200

    Merge branch 'master' of https://github.com/TsvetkovT/MetalSales
    # Conflicts:
    #	.idea/dataSources.xml
    #	.idea/metalsales.iml
    #	.idea/misc.xml
    #	.idea/workspace.xml
    #	static/css/stylepage.css

--- metalsales.py
+++ metalsales.py
@@ -79,32 +79,6 @@ Categories = Categories()
 
 
 
-#define routes
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return  render_template('about.html')
-
-@app.route('/categories')
-def products():
-    # configure MySQL cursor
-    myCursor = mysql.connection.cursor()
-
-    # retreiving data from database
-    myCursor.execute('''SELECT * FROM trade.wp_grupa''')
-    category = myCursor.fetchall()
-
-    return render_template('categories.html', data = category)
-
-
-#error handler
-
-@app.errorhandler(404)
-def error404(error):
-    return render_template('404.html'), 404
 
 #running app parameters
 if __name__ == '__main__':
