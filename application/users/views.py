e319eb499396b48d1f318505c1e279df4529f61d
TsvetkovT <tsvetelin.p.tsvetkov@gmail.com>
2017-12-23 02:08:11 +0200

    login form done

--- application/users/views.py
+++ application/users/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, flash, render_template,url_for,redirect,g
 from .forms import LoginForm
+from flask import current_app as app
 
 import logging
 
@@ -19,5 +20,9 @@ from flask_bcrypt import Bcrypt
 @users_blueprint.route('/login/',methods=['GET',"POST"])
 def login():
     form = LoginForm()
-    return render_template('login.html',form=form)
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/')
+    return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
 
--- instance/config.py
+++ instance/config.py
@@ -3,3 +3,12 @@ import os
 WTF_CSRF_ENABLED = True
 '''config secret key'''
 SECRET_KEY = os.urandom(24)
+
+
+'''configuration of OPEN ID PROVIDERS'''
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
